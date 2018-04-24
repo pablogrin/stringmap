198a33b1578a3a2b1d6a6517066a252fa720e1bd
Pablo Grinberg <pablogrinberg@hotmail.com>
2018-04-24 02:47:28 -0300

    tests added

--- tests/string_map_test.cpp
+++ tests/string_map_test.cpp
@@ -45,3 +45,116 @@ TEST(string_map_test, test_compare) {
     EXPECT_FALSE(m1==m2);
 }
 
+TEST(string_map_test, test_erase) {
+    string_map<int> m1;
+    EXPECT_TRUE(m1.empty());
+    m1["hello"] = 123;
+    EXPECT_TRUE(m1.count("hello"));
+    m1.erase("hello");
+    EXPECT_FALSE(m1.count("hello"));
+    EXPECT_FALSE(m1.count("he"));
+    m1["hello"] = 1;
+    m1["he"] = 3;
+    EXPECT_TRUE(m1.count("hello"));
+    EXPECT_TRUE(m1.count("he"));
+    m1.erase("he");
+    EXPECT_TRUE(m1.count("hello"));
+    EXPECT_FALSE(m1.empty());
+    m1.erase("hello");
+    EXPECT_FALSE(m1.count("hello"));
+    EXPECT_TRUE(m1.empty());
+}
+
+TEST(string_map_test, test_clear) {
+    string_map<int> m1;
+    m1["hello"] = 1;
+    m1["he"] = 3;
+    EXPECT_TRUE(m1.count("hello"));
+    EXPECT_TRUE(m1.count("he"));
+    EXPECT_FALSE(m1.empty());
+    m1.clear();
+    EXPECT_FALSE(m1.count("hello"));
+    EXPECT_FALSE(m1.count("he"));
+    EXPECT_TRUE(m1.empty());
+}
+
+TEST(string_map_test, test_iterator) {
+    string_map<int> m1;
+
+    m1["world"] = 2;
+    m1["aaaa"] = 0;
+    m1["hello"] = 1;
+    /**
+     * m1:  .-a-a-a-a[0]
+     *      |-h-e-l-l-o[1]
+     *      \-w-o-r-l-d[2]
+     */
+    std::list<int> l1, l2 = {0,1,2};
+    for (auto p : m1)
+        l1.push_back(p.second);
+    EXPECT_EQ(l1,l2);
+
+
+    m1["aaaab"] = 12;
+    m1["aaaba"] = 24;
+    m1["aabba"] = 48;
+
+    /**
+     * m1:  .-a-a-a-a[0]-b[12]
+     *      |    | \b-a[24]
+     *      |    \b-b-a[48]
+     *      |-h-e-l-l-o[1]
+     *      \-w-o-r-l-d[2]
+     */
+
+    std::list<int> l3, l4 = {0,12,24,48,1,2};
+    for (auto p : m1)
+        l3.push_back(p.second);
+    EXPECT_EQ(l3,l4);
+
+    EXPECT_EQ(m1.erase("aaaa"), 1);
+
+    /**
+     * m1:  .-a-a-a-a-b[12]
+     *      |    | \b-a[24]
+     *      |    \b-b-a[48]
+     *      |-h-e-l-l-o[1]
+     *      \-w-o-r-l-d[2]
+     */
+
+    auto it = m1.find("aaaab");
+    it = m1.erase(it);
+
+    /**
+     * m1:  .-a-a-a-b-a[24]
+     *      |    \b-b-a[48]
+     *      |-h-e-l-l-o[1]
+     *      \-w-o-r-l-d[2]
+     */
+
+    EXPECT_EQ(it->first, "aaaba");
+    EXPECT_EQ(it->second, 24);
+
+    it = m1.find("aabba");
+    it = m1.erase(it);
+
+    /**
+     * m1:  .-a-a-a-b-a[24]
+     *      |-h-e-l-l-o[1]
+     *      \-w-o-r-l-d[2]
+     */
+    EXPECT_EQ(it, m1.find("hello"));
+
+    EXPECT_EQ(m1.erase(m1.find("world")), m1.end());
+
+    /**
+     * m1:  .-a-a-a-b-a[24]
+     *      \-h-e-l-l-o[1]
+     */
+
+    std::list<int> l5, l6 = {24,1};
+    for (auto p : m1)
+        l5.push_back(p.second);
+    EXPECT_EQ(l5,l6);
+
+}
